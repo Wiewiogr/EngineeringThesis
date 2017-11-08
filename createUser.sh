e36d0b425fa73fa5544740fe544ace33845655c6
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-11-08 21:43:18 +0100

    create user for lxd

--- createUser.sh
+++ createUser.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
 
-
 USER_NAME=$1
 PASSWORD=$2
-LXC_DIR="/lxc"
 
 echo Creating user with name $USER_NAME
-useradd $USER_NAME
+useradd $USER_NAME --groups lxd
 echo "$USER_NAME:$PASSWORD" | chpasswd
 mkdir /home/$USER_NAME
 sudo usermod --shell /bin/bash --home /home/$USER_NAME $USER_NAME
 sudo chown -R "$USER_NAME:$USER_NAME" /home/$USER_NAME
+sudo usermod -G lxd test
+lxc launch ubuntu:16.04 $USER_NAME
+lxc config set $USER_NAME volatile.idmap.next "[]"
+lxc config set $USER_NAME volatile.last_state.idmap "[]"
 
-sudo /usr/sbin/usermod --add-subuids 100000-165536 $USER_NAME
-sudo /usr/sbin/usermod --add-subgids 100000-165536 $USER_NAME
-
-mkdir -p /home/$USER_NAME/.config/lxc
-cat > /home/$USER_NAME/.config/lxc/default.conf <<EOF
-lxc.idmap = u 0 100000 65536
-lxc.idmap = g 0 100000 65536
+cat > /home/$USER_NAME/.profile <<EOF
+lxc exec $USER_NAME bash
+exit
 EOF
-
-mkdir -p /home/$USER_NAME/.local/share/lxc
-chmod -R  a+x /home/$USER_NAME
-
-
-#mkdir $LXC_DIR
-#lxc-create --lxcpath=$LXC_DIR -t download -n $1
-
-
-
-#cat <<EOF
-#sudo lxc-attach --lxcpath $LXC_DIR --name unpriviliged
-#wait
-#exit
-#EOF > /home/$USER_NAME/.profile
-
