29c7f96e102057b00e43b8b46a056cb84d4936d7
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-12-17 18:58:15 +0100

    added toBeDone enpoints

--- LxdUsersManager/app/app.py
+++ LxdUsersManager/app/app.py
@@ -7,15 +7,60 @@ app = Flask(__name__)
 users_manager = users.Users()
 
 
-
 @app.route('/user/<name>/history')
-def user_last_history(name):
-    return jsonify(users_manager.get_user_history(str(name)))
+def user_all_history(name):
+    return jsonify(users_manager.get_user_history(str(name)))  # !!
+
+
+@app.route('/user/<name>/history/elements/<number>')
+def user_last_history(name, number):
+    return jsonify(users_manager.get_user_history(str(name)))  # !!!
+
+
+@app.route('/user/<name>/history/from/<time_from>')
+def user_history_from(name, time_from):
+    pass
+
+
+@app.route('/user/<name>/history/from/<time_from>/to/<time_to>')
+def user_history_from_to(name, time_from, time_to):
+    pass
+
+
+@app.route('/user/<name>/commit/id/<id>')
+def get_changes_in_commit(name, id):
+    pass
+
+
+@app.route('/user/<name>/commit/from/<time_from>/')
+def get_commits_from(name, time_from):
+    pass
+
+
+@app.route('/user/<name>/commit/from/<time_from>/to/<time_to>')
+def get_commits_from_to(name, time_from, time_to):
+    pass
+
+
+@app.route('/user/<name>/file/<file_name>/commit/')
+def get_file_commits_from(name, file_name):
+    pass
+
+
+@app.route('/user/<name>/file/<file_name>/commit/from/<time_from>')
+def get_file_commits_from(name, file_name, time_from):
+    pass
+
+
+@app.route('/user/<name>/file/<file_name>/commit/from/<time_from>/to/<time_to>')
+def get_file_commits_from_to(name, file_name, time_from, time_to):
+    pass
+
 
 # content/<id>
 # listing/tree ls -R
 
-@app.route('/user/<name>/newest_file')     # newest
+@app.route('/user/<name>/newest_file')
 def user_last_modified_file(name):
     return jsonify(users_manager.get_last_modified_file(str(name)))
 
--- LxdUsersManager/app/scriptsExecutor.py
+++ LxdUsersManager/app/scriptsExecutor.py
@@ -4,6 +4,7 @@ import sys
 
 scripts_path = sys.argv[1]
 
+
 def run_script_with_args(script_name, params):
     path = os.path.join(os.getcwd(), scripts_path, script_name)
     return subprocess.check_output(["bash", path] + params)
