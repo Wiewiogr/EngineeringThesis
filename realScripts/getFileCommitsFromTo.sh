7cba2e09e4fa5c2896aca5924419effcee7fe94d
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-12-17 21:55:13 +0100

    getFileCommits script

--- realScripts/getFileCommitsFromTo.sh
+++ realScripts/getFileCommitsFromTo.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 
-if [[ $# -lt 3 ]] ; then
+if [[ $# -lt 2 ]] ; then
     echo "usage: $0 <user> <file> <from> [<to>]"
     exit 1
 fi
 
 user_name=$1
-container_name=C${user_name}
 path_to_repos=/root/repos
 file=$(echo $2 | base64 --decode)
 
+if [[ $# -eq 2 ]] ; then
+    git -C ${path_to_repos}/${user_name} --no-pager shortlog -- ${file}
 if [[ $# -eq 3 ]] ; then
     git -C ${path_to_repos}/${user_name} --no-pager shortlog --since="$(date +"%m.%d.%y %H:%M" -d @$3)" -- ${file}
 elif [[ $# -eq 4 ]] ; then
