f3b70c81a3ffe3caadd5568d7d4d7658235ad6aa
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-12-17 21:40:47 +0100

    implemented file commits

---
+++ realScripts/getFileCommitsFromTo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+if [[ $# -eq 0 ]] ; then
+    echo "usage: $0 <user> <from> <to>"
+    exit 1
+fi
+
+user_name=$1
+container_name=C${user_name}
+path_to_repos=/root/repos
+
+if [[ $# -eq 2 ]] ; then
+    git -C ${path_to_repos}/${user_name} --no-pager shortlog --since="$(date +"%m.%d.%y %H:%M" -d @$2)"
+elif [[ $# -eq 3 ]] ; then
+    git -C ${path_to_repos}/${user_name} --no-pager shortlog --since="$(date +"%m.%d.%y %H:%M" -d @$2)" --until="$(date +"%m.%d.%y %H:%M" -d @$3)"
+fi
