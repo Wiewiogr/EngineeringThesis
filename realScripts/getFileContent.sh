29436d98b5c2565206daa3ce3657565fec9f77b5
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-12-21 02:42:14 +0100

    getFileContent works

--- realScripts/getFileContent.sh
+++ realScripts/getFileContent.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
 
 if [[ $# -lt 2 ]] ; then
     echo "usage: $0 <user> <file> [<id>]"
@@ -10,8 +10,8 @@ path_to_repos=/root/repos
 file=$(echo $2 | base64 --decode)
 
 if [[ $# -eq 2 ]] ; then
-    commit_id=$3
     git -C ${path_to_repos}/${user_name} --no-pager show HEAD:${file}
 else
+    commit_id=$3
     git -C ${path_to_repos}/${user_name} --no-pager show ${commit_id}:${file}
 fi
